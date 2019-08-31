ef14ad6583d1b394b365dfb646cebb7df30cf4ce
victor <guerinonvictor@outlook.fr>
2019-08-31 20:08:18 +0200

    Correctifs title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-8 col-lg-offset-2 title-client" align="center">NOS CLIENT</div>
+        <div class="col-lg-8 col-lg-offset-2 title-client" align="center">NOS CLIENTS</div>
     </div>
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2" id="clients">
