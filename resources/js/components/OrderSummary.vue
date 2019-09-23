2137e2e57a084739c9ee2ed0d3f4bbcf3f87ae34
CrtAurelien <crtaurelien@gmail.com>
2019-09-23 17:26:05 +0200

    push pour vivi :)

--- resources/js/components/OrderSummary.vue
+++ resources/js/components/OrderSummary.vue
@@ -1,21 +1,34 @@
 <template>
-    <div>
-        <div v-for="(selectedProduct, index) in productlist">
-            {{selectedProduct.name}} <i v-on:click="removeProduct(index)" class="material-icons">delete</i>
+    <div class="recap col-lg-2">
+        <h3 class="recap-title">Recapitulatif</h3>
+        <div v-for="(selectedProduct, index) in listproduct" class="recap-products">
+            {{selectedProduct.name}}
+            <i :class="" v-on:click="removeProduct(index)" class="material-icons">delete</i>
         </div>
     </div>
 </template>
+<style>
+    .recap-products{
+
+        background: #4ba593;
+        width: 210px;
+    }
+    .recap-title{
+        background: #4ba593;
+        width: 210px;
+        margin:0 ;
+        padding: 15px 0;
+        text-transform: uppercase;
+        text-align: center;
+    }
+</style>
 <script>
     export default {
-        data(){
-            return{
-                productlist:[{}],
-            }
-        },
         methods:{
             removeProduct: function(index) {
-                this.productlist.splice(index, 1)
-            },
+                this.listproduct.splice(index, 1)
+            }
         },
+        props:['listproduct']
     }
 </script>
