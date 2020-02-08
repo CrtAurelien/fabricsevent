6c419fab50fdcb18240117f66592e64247067f30
victor <guerinonvictor@outlook.fr>
2020-02-08 12:40:45 +0100

    Mise en place de la grille liste de produits

--- resources/js/components/HomeListProducts.vue
+++ resources/js/components/HomeListProducts.vue
@@ -1,28 +1,60 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid homeListProduct">
         <div class="row main_wrapper">
             <div class="col-lg-2 recap">
                 <order-summary></order-summary>
             </div>
             <div class="col-lg-8 middlecolumn">
                 <div class="col-lg-12 main-products">
-                    <div class="col-lg-3" id="homeListProducts" v-for="category in categories" v-if="category.type === 1">
-                        <div class="content">
-                            <div class="test-container">
-                                <div class="deuxB">
-                                    <div class="deuxBUn">
-                                        <figure class="imghvr-shutter-out-diag-2"><img
-                                                :src="'./images/fabrics-img/sweat-fabrics.png'" alt="example-image">
-                                            <figcaption>
-                                                <h3 class="home-categories"><span>{{category.name}}</span></h3>
-                                            </figcaption>
-                                            <router-link :to="{path: '/hkjhk'}"></router-link>
-                                        </figure>
-                                    </div>
-                                </div>
+
+
+                    <div class="col-lg-4">
+                        <figure class="imghvr-shutter-out-diag-2 shadowBox">
+                            <img :src="'./images/fabrics-img/tee-shirt-fabrics.jpg'" alt="example-image">
+                            <figcaption>
+                                <h3 class="home-categories"><span>{{firstCateg.name}}</span></h3>
+                            </figcaption>
+                            <router-link :to="{path: '/hkjhk'}"></router-link>
+                        </figure>
+                    </div>
+
+                    <div class="col-lg-4 flex-product-list">
+                        <div class="container-flex-product">
+                            <div class="flex-product" v-for="category in lCategoriesOne">
+                                <figure class="imghvr-shutter-out-diag-2 shadowBox"><img
+                                        :src="'./images/fabrics-img/autre-fabrics.png'" alt="example-image">
+                                    <figcaption>
+                                        <h3 class="home-categories"><span>{{category.name}}</span></h3>
+                                    </figcaption>
+                                    <router-link :to="{path: '/hkjhk'}"></router-link>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="container-flex-product">
+                            <div class="flex-product" v-for="category in lCategoriesTwo">
+                                <figure class="imghvr-shutter-out-diag-2 shadowBox"><img
+                                        :src="'./images/fabrics-img/autre-fabrics.png'" alt="example-image">
+                                    <figcaption>
+                                        <h3 class="home-categories"><span>{{category.name}}</span></h3>
+                                    </figcaption>
+                                    <router-link :to="{path: '/hkjhk'}"></router-link>
+                                </figure>
                             </div>
                         </div>
                     </div>
+
+
+                    <div class="col-lg-4">
+                        <figure class="imghvr-shutter-out-diag-2 shadowBox">
+                            <img :src="'./images/fabrics-img/tee-shirt-fabrics.jpg'" alt="example-image">
+                            <figcaption>
+                                <h3 class="home-categories"><span>{{lastCateg.name}}</span></h3>
+                            </figcaption>
+                            <router-link :to="{path: '/hkjhk'}"></router-link>
+                        </figure>
+                    </div>
+
+
                 </div>
                 <printmethods></printmethods>
             </div>
@@ -32,10 +64,10 @@
                 <h4 style="text-align: center">Dites-nous tout, votre devis est gratuit ♥</h4>
                 <h4 class="ourclients">Faites comme eux</h4>
                 <div class="col-lg-8 col-lg-offset-2">
-                <div class="ribbon-front">
-                    Ils nous font confiance
-                </div>
-                <div class="ribbon-edge-topleft"></div>
+                    <div class="ribbon-front">
+                        Ils nous font confiance
+                    </div>
+                    <div class="ribbon-edge-topleft"></div>
                     <clients></clients>
                 </div>
                 <div class="col-lg-6 col-lg-offset-3 askdevis">
@@ -43,77 +75,13 @@
                 </div>
             </div>
             <howitworks></howitworks>
-            <!--<div class="col-lg-8" id="homeListProducts">
-                <div class="content">
-                    <div class="test-container">
-                        <div class="un">
-                            <figure class="imghvr-shutter-out-diag-2">
-                                <img :src="'./images/fabrics-img/tee-shirt-fabrics.jpg'" alt="example-image">
-                                <figcaption>
-                                    <h3 class="home-categories"><span>Tee-shirts</span></h3>
-                                </figcaption>
-                                <router-link :to="{path: '/hkjhk'}"></router-link>
-                            </figure>
-                        </div>
-                        <div class="deux">
-                            <div class="deuxA">
-                                <div class="deuxAUn">
-                                    <figure class="imghvr-shutter-out-diag-2"><img
-                                            :src="'./images/fabrics-img/tasse-fabrics.jpg'" alt="example-image">
-                                        <figcaption>
-                                            <h3 class="home-categories"><span>Tasses</span></h3>
-                                        </figcaption>
-                                        <router-link :to="{path: '/hkjhk'}"></router-link>
-                                    </figure>
-                                </div>
-                                <div class="deuxADeux">
-                                    <figure class="imghvr-shutter-out-diag-2"><img
-                                            :src="'./images/fabrics-img/bonnet-fabrics.png'" alt="example-image">
-                                        <figcaption>
-                                            <h3 class="home-categories"><span>Bonnets</span></h3>
-                                        </figcaption>
-                                        <router-link :to="{path: '/hkjhk'}"></router-link>
-                                    </figure>
-                                </div>
-                            </div>
-                            <div class="deuxB">
-                                <div class="deuxBUn">
-                                    <figure class="imghvr-shutter-out-diag-2"><img
-                                            :src="'./images/fabrics-img/sweat-fabrics.png'" alt="example-image">
-                                        <figcaption>
-                                            <h3 class="home-categories"><span>Sweats</span></h3>
-                                        </figcaption>
-                                        <router-link :to="{path: '/hkjhk'}"></router-link>
-                                    </figure>
-                                </div>
-                                <div class="deuxBDeux">
-                                    <figure class="imghvr-shutter-out-diag-2"><img
-                                            :src="'./images/fabrics-img/polo-fabrics.png'" alt="example-image">
-                                        <figcaption>
-                                            <h3 class="home-categories"><span>Polos</span></h3>
-                                        </figcaption>
-                                        <router-link :to="{path: '/hkjhk'}"></router-link>
-                                    </figure>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="trois">
-                            <figure class="imghvr-shutter-out-diag-2"><img
-                                    :src="'./images/fabrics-img/autre-fabrics.png'" alt="example-image">
-                                <figcaption>
-                                    <h3 class="home-categories"><span>Autres</span></h3>
-                                </figcaption>
-                                <router-link :to="{path: '/hkjhk'}"></router-link>
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-            </div>-->
         </div>
     </div>
 </template>
 
 <style scoped>
+
+
 
     .ribbon-wrapper {
         position: relative;z-index:998;
@@ -273,6 +241,7 @@
     .main-products{
         padding-left: 0;
         padding-right: 0;
+        display:flex;
     }
 
     .home-categories {
@@ -282,60 +251,11 @@
         margin: 0;
     }
 
-    .test-container {
-        display: flex;
-        flex-direction: row;
-        text-align: center;
-        /*min-height: 332px;*/
-    }
-
-    .un, .deux, .trois {
-        flex: 1
-    }
-
-    .un + .deux, .deux + .trois, .deuxA + .deuxB {
-        margin-left: 15px;
-    }
-
-
-    .deuxAUn + .deuxADeux, .deuxBUn + .deuxBDeux {
-        margin-top: 15px;
-    }
-
-    .deux {
-        display: flex;
-        flex-direction: row;
-    }
-
-    .deuxA {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .deuxAUn, .deuxADeux {
-        flex: 1;
-    }
-
-
-    .deuxB {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .deuxBUn, .deuxBDeux {
-        flex: 1;
-    }
-
     h3 {
         text-align: center;
     }
 
-    .content {
-        background: transparent;
-        box-shadow: 0px 0 20px -6px #a0a0a0;
-    }
+
 </style>
 
 <script>
@@ -348,13 +268,47 @@
     export default {
         data() {
             return {
-                categories: {}
+                categories: {},
+                lCategoriesOne : [],
+                lCategoriesTwo : [],
+                firstCateg: {},
+                lastCateg: {}
             }
         },
-        created(){
+        methods:  {
+            getCategoriesPhares : function (categ) {
+                for(let i = 0; i<categ.length; i++) {
+                    if(categ[i].type !== 1) {
+                        categ.splice(i, 1);
+                    }
+                }
+                this.spliceCateg(this.categories)
+            },
+            spliceCateg: function(categ) {
+                this.firstCateg = categ[0];
+                categ.splice(0, 1);
+                let length = categ.length - 1;
+                this.lastCateg = categ[length];
+                categ.splice(length, 1);
+
+                for(let i =0; i < categ.length; i++) {
+                    if(i === 0 || i === 1) {
+                        this.lCategoriesOne.push(categ[i]);
+                    } else {
+                        this.lCategoriesTwo.push(categ[i]);
+                    }
+                }
+
+            }
+        },
+        beforeCreate(){
             axios.get('http://localhost/fabricsevent/public/api/categories/')
                 .then((response) => this.categories = response.data)
-                .catch((error) => console.log(error));
+                .catch((error) => console.log(error))
+                .finally(() =>
+                    this.getCategoriesPhares(this.categories)
+                )
+
         },
         components: {
             Clients,
